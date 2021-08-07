d4f6f252b450c0fd6aa0b9f7a5e9ab26c15f6d80
Gabriel Tonioni Duarte <gtonioni13@gmail.com>
2021-08-07 19:39:26 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -4,19 +4,31 @@ public class HtmlStatement extends Statement {
     
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-       String result = "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
-       while(rentals.hasMoreElements()) {
-           Rental each = (Rental) rentals.nextElement();
-           // show figures for each rental
-           result += each.getMovie().getTitle() + ": " +
-                     String.valueOf(each.getCharge()) + "<BR>\n";
-       }
-       // add footer lines
-       result += "<P>You owe <EM>" + 
-       String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-       result += "On this rental you earned <EM>" +
-       String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-       "</EM> frequent renter points<P>";
+        String result = getRecord(aCustomer);
+        while(rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            //show figures for this rental
+            result += getFigures(each);
+        }
+        //add footer lines
+        result +=  getFooter(aCustomer);
         return result;
+    }
+
+    public String getRecord(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+    }
+
+    public String getFigures(Rental aRental) {
+        return aRental.getMovie().getTitle() + ": " +
+               String.valueOf(aRental.getCharge()) + "<BR>\n";
+    }
+
+    public String getFooter(Customer aCustomer) {
+        return "<P>You owe <EM>" + 
+               String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n" +
+               "On this rental you earned <EM>" +
+               String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+               "</EM> frequent renter points<P>";
     }
 }
